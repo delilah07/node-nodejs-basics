77c07f8b08552e9f5ff52fc01ca2966869d617e5
unknown <you@example.com>
2022-06-04 23:32:24 +0200

    feat: add solutions for hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,17 @@
-export const calculateHash = async () => {
-    // Write your code here 
+import crypto from "crypto";
+import { readFile } from "fs";
+import path from "path";
+
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const calculateHash = async (file) => {
+  // Write your code here
+  const filePath = path.join(`${__dirname}/files/`, file);
+  readFile(filePath, "utf8", (err, data) => {
+    if (err) throw err;
+    console.log(crypto.createHash("sha256").update(data).digest("hex"));
+  });
 };
+calculateHash("fileToCalculateHashFor.txt");
